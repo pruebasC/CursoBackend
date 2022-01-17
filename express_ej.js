cea7240ec7fcace80c21954a6b44736e84125838
gabriel_cgn2@hotmail.com <gabriel_cgn2@hotmail.com>
2022-01-16 21:12:36 -0300

    se agrego el post con body-parses

--- express_ej.js
+++ express_ej.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extented: true}));
 
 app.get('/saludo',function(req,res){
   res.send(`Hola ${req.query.name}`);
+});
+
+app.post('/',function(req,res){
+  res.send(`Hola ${req.body.name}`);
 });
 
 app.listen(3000);
